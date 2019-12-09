0e2a38db509cc01ead1ea446a1657897f81a8f66
Adam Krupa <adamkrupa96@gmail.com>
2019-12-09 20:45:28 +0100

    initial commit (little shooter game)

---
+++ cmake-build-debug/CMakeFiles/little_shooter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/little_shooter.dir/main.cpp.o"
+  "little_shooter"
+  "little_shooter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/little_shooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
